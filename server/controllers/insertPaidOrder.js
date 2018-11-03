13cff932eb6ecef628e0ddfea335b77e4be33299
barbosa_home <barbosa90@126.com>
2018-11-03 14:14:11 +0800

    call insert_paid_order

--- server/controllers/insertPaidOrder.js
+++ server/controllers/insertPaidOrder.js
@@ -19,11 +19,12 @@ var query = (params) => {
     connection.connect(function (err) {
       if (err != null) console.log(err)
     })
-    var selectString1 = "SELECT * FROM `Pre_order` WHERE `Pre_order`.`id` = ? FOR UPDATE;" ;
-    var selectString2 = "UPDATE `Pre_order` SET `payStatus` = '1' WHERE `Pre_order`.`id` = ?;";
-    var selectString3 = "INSERT INTO `Paid_order` (`USERID`, `ORDERID`, `MERCHID`, `PAIDTIME`, `STATUS`, `DISCOUNT`, `POINTSUSED`, `POINTS`, `MERCHCOST`, `FINALCOST`, `AMOUNT`, `ADDRESS`, `PHONE`, `DEADLINE`) VALUES (?, ?, ?, CURRENT_TIMESTAMP, b'0', ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    // var selectString1 = "SELECT * FROM `Pre_order` WHERE `Pre_order`.`id` = ? FOR UPDATE;" ;
+    // var selectString2 = "UPDATE `Pre_order` SET `payStatus` = '1' WHERE `Pre_order`.`id` = ?;";
+    // var selectString3 = "INSERT INTO `Paid_order` (`USERID`, `ORDERID`, `MERCHID`, `PAIDTIME`, `STATUS`, `DISCOUNT`, `POINTSUSED`, `POINTS`, `MERCHCOST`, `FINALCOST`, `AMOUNT`, `ADDRESS`, `PHONE`, `DEADLINE`) VALUES (?, ?, ?, CURRENT_TIMESTAMP, b'0', ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    var selectString = "CALL insert_paid_order(?,?,?,?,?,?,?,?,?,?,?)";
     //查询订单，更改订单状态，插入支付订单：用户id，订单id，商品id，支付时间，状态，折扣，使用积分，获得积分，商品价，最终价，数量，地址，电话，最晚使用时间可为空
-    var paramsArr = [params.orderid, params.orderid, params.userid, params.orderid, params.merchid, params.discount, params.pointsused, params.points, params.merchcost, params.finalcost, params.amount, params.address, params.phone, params.deadline]
+    var paramsArr = [params.orderid, params.userid, params.merchid, params.discount, params.pointsused, params.points, params.merchcost, params.finalcost, params.amount, params.address, params.phone]
     connection.query(selectString, paramsArr, function (err, rows, fields) {
 
       if (err != null) reject(err)
